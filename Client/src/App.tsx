37901e56973d66882207a8d0267389a247ee7f00
Iain Stewart <iainstewart08@gmail.com>
2022-10-31 09:00:41 +0000

    Add cors and call api using fetch

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [products, setProducts] = useState([
     { name: 'product1', price: 100.0 },
     { name: 'product2', price: 200.0 },
   ]);
+
+  useEffect(() => {
+    fetch('https://localhost:5001/api/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   function addProduct() {
     setProducts((prevState) => [
