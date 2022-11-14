11b205b4810d3a787f1dae50e191c74cf35f36a0
Iain Stewart <iainstewart08@gmail.com>
2022-11-14 08:12:43 +0000

    Add navlinks

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
