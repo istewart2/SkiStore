f1df65936aec1fd17827a5ab4a3355c34b608fe7
istewart2 <iain.stewart@kpmg.co.uk>
2023-08-26 11:21:58 +0100

    Remove unused classes + imports

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,8 +2,6 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace API.Controllers
 {
