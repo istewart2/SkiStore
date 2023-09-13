78beca200e8fa735b3b874ac8b30c131c2ca55b4
istewart2 <iain.stewart@kpmg.co.uk>
2023-09-13 09:24:45 +0100

    Catch errors thrown by test errors

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -22,7 +22,13 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProductAsync(int id)
         {
-            return await _context.Products.FindAsync(id);
+            var product = await _context.Products.FindAsync(id);
+
+            if (product is null) {
+                return NotFound();
+            }
+
+            return product;
         }
     }
 }
